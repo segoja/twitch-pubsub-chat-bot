750d488f6e6036fcdc357b77ad8b175c266389b8
deke <deke@dekes-MacBook-Pro.local>
2020-03-15 15:59:21 -0400

    Dev keys

---
+++ config/dev.sample.js
@@ -0,0 +1,8 @@
+module.exports = {
+    ACCESS_TOKEN: 'TWITCH_ACCESS_TOKEN',
+    REFRESH_TOKEN: 'TWITCH_REFRESH_TOKEN',
+    TWITCH_CLIENT_ID: 'TWITCH_CLIENT_ID',
+    TWITCH_CLIENT_SECRET: 'TWITCH_CLIENT_SECRET',
+    MONGO_URI: 'mongoDB_url', // Either localhost or site such as: mlab.com
+  };
+  
